e75a6d43081fd79859f036af4dbb3eb867183447
mr.javaria.y2b@gmail.com <mr.javaria.y2b@gmail.com>
2024-06-27 12:20:29 +0500

    dashboard complete

---
+++ src/lib/redis.ts
@@ -0,0 +1,6 @@
+import { Redis } from '@upstash/redis'
+
+export const redis = new Redis({
+  url: 'https://full-sole-52349.upstash.io',
+  token: 'Acx9AAIncDFkYjMyODQ3MjUxYmY0ZjdmYTgxNmU0YzNjMjM2N2RiZHAxNTIzNDk',
+})
